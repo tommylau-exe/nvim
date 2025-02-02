971d67e1a3ab8aeb78a100cf71daa6593556f454
Tom <tom@lauerman.dev>
2025-02-02 13:05:15 -0800

    use <Tab> to confirm autocomplete

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -3,7 +3,7 @@ return {
     version = 'v0.11',
     dependencies = 'rafamadriz/friendly-snippets',
     opts = {
-        keymap = { preset = 'default' },
+        keymap = { preset = 'super-tab' },
         appearance = {
             use_nvim_cmp_as_default = true,
         },
