7cd8bd54d32a1b73b04efcfde4b764a0191c6ceb
tommylau-exe <tom@lauerman.dev>
2025-09-21 19:05:53 -0700

    reorganize plugins to local and remote

---
+++ lua/plugins/remote/init.lua
@@ -0,0 +1,18 @@
+-- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
+local path_package = vim.fn.stdpath('data') .. '/site'
+local mini_path = path_package .. '/pack/deps/start/mini.nvim'
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = {
+    'git', 'clone', '--filter=blob:none',
+    'https://github.com/nvim-mini/mini.nvim', mini_path
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+end
+require('mini.deps').setup()
+
+require('plugins.remote.mini')
+require('plugins.remote.theme')
+require('plugins.remote.treesitter')
